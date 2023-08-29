5bc72c99c62c7f01e7c004470da84fb34d679dcc
Adalbert047 <aadalberto835@gmail.com>
2023-08-29 11:30:43 -0500

    Commit del dia de ayer desde laptop

--- Curso JS/Leccion09/Clases.js
+++ Curso JS/Leccion09/Clases.js
@@ -1,7 +1,4 @@
-class Persona {
-
-
-    
+class Persona {    
     constructor(nombre, apellido, email, edad, sexo)
     {
         this.nombre = nombre;
@@ -19,6 +16,11 @@ class Persona {
     AñosaFuturo = function(n) 
     {
         return this.edad + n;
+    }
+
+    toString()
+    {
+        return this.nombreCompleto + ", Edad : " + this.edad + ", Sexo : " + this.sexo;
     }
 }
 
@@ -38,3 +40,62 @@ console.log(Yo.estadoCivil);
 console.log(Yo.AñosaFuturo(5));
 
 
+
+/*Herencia en JS*/
+
+class Empleado extends Persona
+{
+    constructor(nombre, apellido, email, edad, sexo, departamento)
+    {
+        /*Se debe pasar los parametros de la clase hija en super*/
+        super(nombre, apellido, email, edad, sexo);
+        this.departamento = departamento;
+
+    }
+
+    get Departamento (){
+        return this.departamento;
+    }
+
+    set Departamento(departamento) {
+        this.departamento = departamento
+    }
+
+    /*Sobreescritura en JS*/
+
+    //Sigue siendo similar a C#
+    get nombreCompleto()
+    {
+        return super.nombreCompleto + ", Departamento: " + this.departamento;
+    }
+
+
+    toString()
+    {
+        return super.toString() + ", Departamento: " + this.departamento;
+    }
+
+
+    /*Con 'super' se puede llamar los valores, metodos y propiedades de la clase padre para su uso en la clase hijo*/
+}
+
+
+let empleadin = new Empleado("Juanito", "Fulanito", "juanuto@email.com", 20, "M", "Recursos Humanos");
+
+
+console.log(empleadin.Departamento);
+empleadin.Departamento = "Psicologia";
+
+console.log(empleadin.Departamento);
+
+empleadin;
+
+
+console.log(empleadin.AñosaFuturo(5));
+console.log(empleadin.nombreCompleto);
+
+
+
+/*Polimorfismo, se llama cuando un objeto llama el metodo de una clase padre
+  (Multiple formas en tiempo de ejecucion)*/
+console.log(empleadin.toString());
